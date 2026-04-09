191f30cab81115141cf5046deddc6b493c3d454e
Meghana Thota <meghana@meghanas-mbp.home>
2026-04-09 19:58:46 -0400

    changed the file context

--- src/show_environment.sh
+++ src/show_environment.sh
@@ -16,24 +16,4 @@ else
 fi
 
 echo
-echo#!/bin/bash
-
-(
-  set -x
-
-  env | sort
-
-  uname -a
-)
-
-if [[ -x "$(command -v node)" ]]; then
-  ( set -x; node --version )
-else
-  echo "WARN: NodeJS not installed"
-fi
-
-if [[ -x "$(command -v yarn)" ]]; then
-  ( set -x; yarn --version )
-else
-  echo "WARN: Yarn not installed"
-fi
+echo
